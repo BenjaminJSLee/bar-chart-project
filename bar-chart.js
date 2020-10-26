1fd20f72a6e0a819a66b9795e6bea5462ac85a45
Benjamin Lee <benjaminjslee@gmail.com>
2020-10-26 05:44:40 +0000

    added default values for options that were not included in the user input

--- bar-chart.js
+++ bar-chart.js
@@ -10,34 +10,33 @@ const getDataRange = (arr) => {
 
 const createBars = (data, opts, width, height) => {
   let heightCalc;
-  let barWidth = width / data.length;
+  let barWidth = opts.width / data.length;
   let minMax = getDataRange(data) || [1,-1];
   let barsContainer = $('<div>').css({
-    height: height,
+    height: opts.height,
   });
   if( minMax[1] - minMax[0] === 0 ) heightCalc = undefined;
   else if( minMax[1] - minMax[0] < 0 ) return undefined;
   else lengthCalc = (val,minMax) => (val - minMax[0]) / (minMax[1] - minMax[0]);
   for(let val of data){
     let bar, txt;
-    let barHeight = ((height-20) * (lengthCalc ? lengthCalc(val,minMax) : 0.5)) + 20;
+    let barHeight = ((opts.height-opts.height/10) * (lengthCalc ? lengthCalc(val,minMax) : 0.5)) + opts.height/10;
     bar = $('<div>')
       .css({
       position: "relative",
       textAlign: "center",
       display: "inline-block",
-      backgroundColor: opts["colour"] || "blue",
+      backgroundColor: opts.barColour,
       width: barWidth,
       height: barHeight,
-      marginLeft: opts["barSpace"] || 10
+      marginLeft: opts.barSpace
     });
     txt = $("<div>").css({
       position: "absolute",
       width: "auto",
-      top: opts["top"] || null,
       left: "50%",
-      bottom: opts["bottom"] || null,
-      transform: "translate(-50%,-" + (opts["top"] || "0%") +  ")"
+      bottom: opts.dataPlace,
+      transform: "translateX(-50%)"
     }).html(val);
     bar.append(txt);
     barsContainer.append(bar);
@@ -49,14 +48,14 @@ const createXAxis = (data, opts, width) => {
   let labels = opts.labels, axisContainer, metadata;
   axisContainer = $('<div>').css({
     display: "block",
-    width: width + opts["barSpace"] * data.length + 10,
-    height: 20 - opts["lineWidth"],
+    width: opts.width + opts.barSpace * data.length + 10,
+    height: 20 - opts.lineWidth,
     borderTop: "1px solid black"
   });
   axisContainer.append($('<div>').css({
     display: "inline-block",
-    width: 10 - opts["lineWidth"],
-    height: 20 - opts["lineWidth"],
+    width: 10 - opts.lineWidth,
+    height: 20 - opts.lineWidth,
     borderRight: "1px solid black"
   }));
   for(let label of labels){
@@ -64,11 +63,11 @@ const createXAxis = (data, opts, width) => {
       position: "relative",
       display: "inline-block",
       textAlign: "center",
-      width: (width / labels.length) - 2,
-      height: 20 - opts["lineWidth"],
+      width: (opts.width / data.length) - 2,
+      height: 20 - opts.lineWidth,
       borderLeft: "1px solid black",
       borderRight: "1px solid black",
-      marginLeft: opts["barSpace"]
+      marginLeft: opts.barSpace
     });
     text = $('<div>').css({
       position: "absolute",
@@ -80,7 +79,7 @@ const createXAxis = (data, opts, width) => {
   return axisContainer;
 };
 
-const createYAxis = (data,opts,height) => {
+const createYAxis = (data,opts) => {
   let labels = opts.labels, axisContainer, metadata;
   axisContainer = $('<div>').css({
     position: "relative",
@@ -92,7 +91,7 @@ const createYAxis = (data,opts,height) => {
     metadata = $('<div>').css({
       display: "block",
       textAlign: "center",
-      height: (height / 10) - 1,
+      height: (opts.height / 10) - 1,
       borderTop: "1px solid black",
       borderRight: "1px solid black"
     });
@@ -102,15 +101,41 @@ const createYAxis = (data,opts,height) => {
 };
 
 const createGraphBox = (data,options) => {
-  let graphContainer, yAxis, xAxis, bars,
-  width = options["width"] || 500, height = options["height"] || 500;
-  if( !(options["barSpace"]) ) options["barSpace"] = 10;
+  let graphContainer, yAxis, xAxis, bars;
   graphContainer = $('<div>');
-  xAxis = createXAxis(data,options,width);
-  yAxis = createYAxis(data,options,height);
-  bars = createBars(data,options,width,height);
+  xAxis = createXAxis(data,options);
+  yAxis = createYAxis(data,options);
+  bars = createBars(data,options);
   graphContainer.append(yAxis,bars,xAxis);
   return graphContainer;
+};
+
+/** Takes the user inputted options and replaces the default values with the
+ * inputted values.
+ *
+ * @param {*} opts
+ */
+const getDefaultOpts = (data,opts) => {
+  let defaultOpts = {
+    // put all default options here
+    width: 500,
+    height: 500,
+    title: "Bar Chart",
+    titleSize: "16px",
+    titleColour: "black",
+    barSpace: 2,
+    lineWidth: 1,
+    dataPlace: "100%",
+    barColour: "red",
+    labels: data,
+    labelColour: "white",
+    xAxisName: 'x',
+    yAxisName: 'y'
+  };
+  for(let prop in opts){
+    if( defaultOpts[prop] ) defaultOpts[prop] = opts[prop];
+  }
+  return defaultOpts;
 };
 
 /**
@@ -120,10 +145,14 @@ const createGraphBox = (data,options) => {
  * @param {jQuery element} element is the container for the bar chart
  */
 const drawBarChart = (data, options, element) => {
-  let title, graphBox;
+  let title, graphBox, allOpts;
+  allOpts = getDefaultOpts(data,options);
   title = $("<div>").css({
-
-  }).html(options["title"] || "Bar Chart");
-  graphBox = createGraphBox(data,options);
+    textAlign: "center",
+    margin: "auto",
+    fontSize: options["titleSize"],
+    fontColour: options["titleColour"]
+  }).html(allOpts["title"]);
+  graphBox = createGraphBox(data,allOpts);
   element.append(title,graphBox);
 };
