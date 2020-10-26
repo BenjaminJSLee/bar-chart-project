993d317aa0a528293149988e8f18e03d7ab87f7c
Benjamin Lee <benjaminjslee@gmail.com>
2020-10-26 04:56:16 +0000

    added graph outline and metadata implementation for the bars in bar-chart.js (rough draft)

--- bar-chart.js
+++ bar-chart.js
@@ -8,29 +8,109 @@ const getDataRange = (arr) => {
   return [min,max];
 };
 
-const createBars = (data, opts) => {
-  let barWidth = opts.conWidth / data.length;
+const createBars = (data, opts, width, height) => {
+  let heightCalc;
+  let barWidth = width / data.length;
   let minMax = getDataRange(data) || [1,-1];
-  let bars = [], heightCalc;
+  let barsContainer = $('<div>').css({
+    height: height,
+  });
   if( minMax[1] - minMax[0] === 0 ) heightCalc = undefined;
   else if( minMax[1] - minMax[0] < 0 ) return undefined;
   else lengthCalc = (val,minMax) => (val - minMax[0]) / (minMax[1] - minMax[0]);
   for(let val of data){
-    let barHeight = opts.conHeight * (lengthCalc ? lengthCalc(val,minMax) : 0.5);
-    bars.push($('<div>')
+    let bar, txt;
+    let barHeight = ((height-20) * (lengthCalc ? lengthCalc(val,minMax) : 0.5)) + 20;
+    bar = $('<div>')
       .css({
+      position: "relative",
+      textAlign: "center",
       display: "inline-block",
-      backgroundColor: "red",
+      backgroundColor: opts["colour"] || "blue",
       width: barWidth,
       height: barHeight,
-      marginLeft: 10
-    }));
+      marginLeft: opts["barSpace"] || 10
+    });
+    txt = $("<div>").css({
+      position: "absolute",
+      width: "auto",
+      top: opts["top"] || null,
+      left: "50%",
+      bottom: opts["bottom"] || null,
+      transform: "translate(-50%,-" + (opts["top"] || "0%") +  ")"
+    }).html(val);
+    bar.append(txt);
+    barsContainer.append(bar);
   }
-  return bars;
+  return barsContainer;
 };
 
-const createGraphBox = () => {
+const createXAxis = (data, opts, width) => {
+  let labels = opts.labels, axisContainer, metadata;
+  axisContainer = $('<div>').css({
+    display: "block",
+    width: width + opts["barSpace"] * data.length + 10,
+    height: 20 - opts["lineWidth"],
+    borderTop: "1px solid black"
+  });
+  axisContainer.append($('<div>').css({
+    display: "inline-block",
+    width: 10 - opts["lineWidth"],
+    height: 20 - opts["lineWidth"],
+    borderRight: "1px solid black"
+  }));
+  for(let label of labels){
+    metadata = $('<div>').css({
+      position: "relative",
+      display: "inline-block",
+      textAlign: "center",
+      width: (width / labels.length) - 2,
+      height: 20 - opts["lineWidth"],
+      borderLeft: "1px solid black",
+      borderRight: "1px solid black",
+      marginLeft: opts["barSpace"]
+    });
+    text = $('<div>').css({
+      position: "absolute",
+      top: 0
+    }).html(label);
+    metadata.append(text);
+    axisContainer.append(metadata);
+  }
+  return axisContainer;
+};
 
+const createYAxis = (data,opts,height) => {
+  let labels = opts.labels, axisContainer, metadata;
+  axisContainer = $('<div>').css({
+    position: "relative",
+    display: "inline-block",
+    float: "left",
+    width: "10"
+  });
+  for(let i = 0; i < 10; i++){
+    metadata = $('<div>').css({
+      display: "block",
+      textAlign: "center",
+      height: (height / 10) - 1,
+      borderTop: "1px solid black",
+      borderRight: "1px solid black"
+    });
+    axisContainer.append(metadata);
+  }
+  return axisContainer;
+};
+
+const createGraphBox = (data,options) => {
+  let graphContainer, yAxis, xAxis, bars,
+  width = options["width"] || 500, height = options["height"] || 500;
+  if( !(options["barSpace"]) ) options["barSpace"] = 10;
+  graphContainer = $('<div>');
+  xAxis = createXAxis(data,options,width);
+  yAxis = createYAxis(data,options,height);
+  bars = createBars(data,options,width,height);
+  graphContainer.append(yAxis,bars,xAxis);
+  return graphContainer;
 };
 
 /**
@@ -40,8 +120,10 @@ const createGraphBox = () => {
  * @param {jQuery element} element is the container for the bar chart
  */
 const drawBarChart = (data, options, element) => {
-  let title, bars, width = 500, height = 500;
-  if( options["width"] ) width = Number(options.width);
-  if( options["height"] ) height = Number(options.height);
-  element.append(createBars(data,{conWidth: width, conHeight: height}));
+  let title, graphBox;
+  title = $("<div>").css({
+
+  }).html(options["title"] || "Bar Chart");
+  graphBox = createGraphBox(data,options);
+  element.append(title,graphBox);
 };
