9bf1e64076507437d9cb437008f4fc950ccaef15
Benjamin Lee <benjaminjslee@gmail.com>
2020-10-30 04:58:15 +0000

    fixed allignment issues with axis and added more customisability ( specifically with the number of ticks in the y-axis )

--- bar-chart.js
+++ bar-chart.js
@@ -1,36 +1,36 @@
 const getDataRange = (arr) => {
-  if( arr.length < 0 ) return undefined;
+  if (arr.length < 0) return undefined;
   let max = arr[0], min = arr[0];
-  for( let val of arr ){
-    if(max < val) max = val;
-    else if(min > val) min = val;
+  for (let val of arr) {
+    if (max < val) max = val;
+    else if (min > val) min = val;
   }
   return [min,max];
 };
 
-const createBars = (data, opts, width, height) => {
-  let heightCalc;
+const createBars = (data, opts) => {
+  let lengthCalc;
   let barWidth = opts.width / data.length;
   let minMax = getDataRange(data) || [1,-1];
   let barsContainer = $('<div>').css({
     height: opts.height,
   });
-  if( minMax[1] - minMax[0] === 0 ) heightCalc = undefined;
-  else if( minMax[1] - minMax[0] < 0 ) return undefined;
+  if (minMax[1] - minMax[0] === 0) lengthCalc = undefined;
+  else if (minMax[1] - minMax[0] < 0) return undefined;
   else lengthCalc = (val,minMax) => (val - minMax[0]) / (minMax[1] - minMax[0]);
-  for(let val of data){
+  for (let val of data) {
     let bar, txt;
-    let barHeight = ((opts.height-opts.height/10) * (lengthCalc ? lengthCalc(val,minMax) : 0.5)) + opts.height/10;
+    let barHeight = ((opts.height - opts.height / opts.ticks) * (lengthCalc ? lengthCalc(val,minMax) : 0.5)) + opts.height / opts.ticks;
     bar = $('<div>')
       .css({
-      position: "relative",
-      textAlign: "center",
-      display: "inline-block",
-      backgroundColor: opts.barColour,
-      width: barWidth,
-      height: barHeight,
-      marginLeft: opts.barSpace
-    });
+        position: "relative",
+        textAlign: "center",
+        display: "inline-block",
+        backgroundColor: opts.barColour,
+        width: barWidth,
+        height: barHeight,
+        marginLeft: opts.barSpace
+      });
     txt = $("<div>").css({
       position: "absolute",
       width: "auto",
@@ -44,33 +44,36 @@ const createBars = (data, opts, width, height) => {
   return barsContainer;
 };
 
-const createXAxis = (data, opts, width) => {
+const createXAxis = (data, opts) => {
   let labels = opts.labels, axisContainer, metadata;
   axisContainer = $('<div>').css({
+    position: "relative",
     display: "block",
-    width: opts.width + opts.barSpace * data.length + 10,
-    height: 20 - opts.lineWidth,
+    width: opts.width + opts.barSpace * data.length + data.length * opts.lineWidth * 2 - 2,
+    height: opts.tickLength - opts.lineWidth,
     borderTop: "1px solid black"
   });
   axisContainer.append($('<div>').css({
+    position: "relative",
     display: "inline-block",
-    width: 10 - opts.lineWidth,
-    height: 20 - opts.lineWidth,
+    verticalAlign: "top",
+    width: opts.tickLength - opts.lineWidth,
+    height: opts.tickLength - opts.lineWidth,
     borderRight: "1px solid black"
   }));
-  for(let label of labels){
+  for (let label of labels) {
     metadata = $('<div>').css({
       position: "relative",
       display: "inline-block",
       textAlign: "center",
-      width: (opts.width / data.length) - 2,
-      height: 20 - opts.lineWidth,
+      width: (opts.width / data.length) - opts.lineWidth * 2,
+      height: opts.tickLength - opts.lineWidth,
       borderLeft: "1px solid black",
       borderRight: "1px solid black",
       marginLeft: opts.barSpace
     });
     text = $('<div>').css({
-      position: "absolute",
+      position: "relative",
       top: 0
     }).html(label);
     metadata.append(text);
@@ -87,11 +90,11 @@ const createYAxis = (data,opts) => {
     float: "left",
     width: "10"
   });
-  for(let i = 0; i < 10; i++){
+  for (let i = 0; i < opts.ticks; i++) {
     metadata = $('<div>').css({
       display: "block",
       textAlign: "center",
-      height: (opts.height / 10) - 1,
+      height: (opts.height / opts.ticks) - 1,
       borderTop: "1px solid black",
       borderRight: "1px solid black"
     });
@@ -127,13 +130,15 @@ const getDefaultOpts = (data,opts) => {
     lineWidth: 1,
     dataPlace: "100%",
     barColour: "red",
-    labels: Array(data.length).fill().map((val,i)=>i+1),
+    labels: Array(data.length).fill().map((val,i) => i + 1),
     labelColour: "white",
     xAxisName: 'x',
-    yAxisName: 'y'
+    yAxisName: 'y',
+    ticks: 10,
+    tickLength: 10
   };
-  for(let prop in opts){
-    if( defaultOpts[prop] ) defaultOpts[prop] = opts[prop];
+  for (let prop in opts) {
+    if (defaultOpts[prop]) defaultOpts[prop] = opts[prop];
   }
   return defaultOpts;
 };
